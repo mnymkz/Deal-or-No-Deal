c42ab4007424a9300c83b9a07ad4e5f67d6b3799
tley4 <105393760+tley4@users.noreply.github.com>
2023-08-22 23:41:40 +1200

    Instantiate Banker

--- src/Banker/Banker.java
+++ src/Banker/Banker.java
@@ -3,8 +3,33 @@ package Banker;
 
 /**
  *
- * @author Michael
+ * @author Tabitha
  */
-public class Banker {
+public class Banker 
+{
+    private String name;
+    private double offer;
 
+    public Banker(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getOffer()
+    {
+        return offer;
+    }
+
+    public void setOffer(double offer)
+    {
+        this.offer = offer;
+    }
+
+    //make a function to calculate logic of the game
+    //consult about how we want the rules of the game to be, if we want to alter anything
 }
