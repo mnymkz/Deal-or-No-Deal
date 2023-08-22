b79d8a8637f7dcb256d8037bc100484366f77a89
tley4 <105393760+tley4@users.noreply.github.com>
2023-08-22 23:13:26 +1200

    Briefcase update
    instantiating the object and the number of the briefcase

--- src/Case/Case.java
+++ src/Case/Case.java
@@ -3,8 +3,32 @@ package Case;
 
 /**
  *
- * @author Michael
+ * @author Tabitha
  */
-public class Case {
+public class Case 
+{
+    private int number;
+    private String object;
 
+    public Case(int number, String object)
+    {
+        this.number = number;
+        this.object = object;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public String getObject()
+    {
+        return object;
+    }
+
+    @Override
+    public String toString()
+    {
+        return number + ": " + object;
+    }
 }
