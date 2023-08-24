db224579b16494cec4b68ee0e3723cda9232c8cc
tley4 <105393760+tley4@users.noreply.github.com>
2023-08-24 17:48:20 +1200

    Update Round.java

--- src/Main/Round.java
+++ src/Main/Round.java
@@ -1,10 +1,41 @@
-
 package Main;
 
+import java.util.List;
+import Banker.Banker;
+import Case.Case;
+import Player.Player;
+import java.util.ArrayList;
 /**
  *
- * @author Michael
+ * @author Tabitha
  */
-public class Round {
+public class Round 
+{
+    private ArrayList<Case> casesRemaining = new ArrayList<Case>();
+    private Player player;
+    private Banker banker;
 
+    //constructor
+    public Round(ArrayList<Case> casesRemaining, Player player, Banker banker)
+    {
+        this.casesRemaining = new ArrayList<Case>();
+        this.player = player;
+        this.banker = banker;
+    }
+
+    public double averageRemainingCases()
+    {
+        int remainingCases = 0;
+        double totalCasesValue = 0;
+
+        for(int i = 0; casesRemaining > 0; i++)
+        {
+            if()
+        }
+    }
+
+    public void start()
+    {
+        
+    }
 }
