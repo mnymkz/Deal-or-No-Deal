7d9dfe020e1137043f0c4430d0dd7a6da1d596ed
tley4 <105393760+tley4@users.noreply.github.com>
2023-08-29 10:30:52 +1200

    Making the base for AggresiveBanker

--- src/Banker/AggressiveBanker.java
+++ src/Banker/AggressiveBanker.java
@@ -3,8 +3,18 @@ package Banker;
 
 /**
  *
- * @author Michael
+ * @author Tabitha
  */
-public class AggressiveBanker {
+public class AggressiveBanker extends Banker
+{
+    public AggressiveBanker(String name)
+    {
+        super(name, "Aggresive");
+    }
 
+    @Override
+    public double bankerOffer(double average)
+    {
+        return average * 0.8;
+    }
 }
