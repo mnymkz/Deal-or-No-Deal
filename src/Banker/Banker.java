aa5751e7c424ed1786cb7769529045741d386fd3
tley4 <105393760+tley4@users.noreply.github.com>
2023-08-30 10:37:42 +1200

    Created 'createOffer'

--- src/Banker/Banker.java
+++ src/Banker/Banker.java
@@ -1,6 +1,12 @@
 
 package Banker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Case.Case;
+import Case.Item;
+
 /**
  *
  * @author Tabitha
@@ -44,7 +50,41 @@ public class Banker
         this.strategy = strategy;
     }
 
+
+    public double createOffer(int round, int playerAmount, ArrayList<Case> cases)
+    {
+        int totalValueItems = 0;
+        int numItems = 0;
+        
+        //iterating through every opened case
+        for(Case currentCase : cases)
+        {
+            if(currentCase.isOpened())
+            {
+                Item items = currentCase.getItem();
+                //why isnt items working???
+                for (Item item : items) {
+                    totalValueItems += item.getMoneyValue();
+                    numItems++;
+                }
+            }
+        }
+
+        //calculating the average of the value of the items 
+        double valueOfItemAverage;
+        if(numItems > 0)
+        {
+            valueOfItemAverage = (double) totalValueItems / numItems;
+        }
+        else 
+        {
+            valueOfItemAverage = 0;
+        }
+
+        //make calculation based on the player on the round, player amount and the average value of the items 
+    }
     
+    //to be overriden by extended classes
     public double bankerOffer(double average)
     {
         return 0;
