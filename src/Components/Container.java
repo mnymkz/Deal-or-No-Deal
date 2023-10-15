0b0cb512e86ef0f2870327c109aeac3bfddabee6
tley4 <105393760+tley4@users.noreply.github.com>
2023-10-16 00:48:42 +1300

    Adding new constructors for Container class

--- src/Components/Container.java
+++ src/Components/Container.java
@@ -1,15 +1,42 @@
 package Components;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Color;
 
 
 public class Container 
 {
+    /**
+     * Creates a Container panel including the specified width and height
+     * Sets backgorund to dark grey
+     * 
+     * @param width
+     * @param height
+     */
     public Container(int width, int height)
     {
         setPrefferedSize(new Dimension(width, height));
-        setBackground(Color.darkGray);
+        this.setBackground(Color.darkGray);
     }
+
+    /**
+     * Creates a Container panel including specified alignment, width and height
+     * Components are added with specific alignment within the panel
+     * 
+     * @param align
+     * @param width
+     * @param height
+     * @param components
+     */
+    public Container(int align, int width, int height, JComponent... components)
+    {
+        this(width, height);
+        this.setLayout(new FlowLayout(align));
+        for (JComponent i : components) this.add(i);
+    }
+
+
 }
