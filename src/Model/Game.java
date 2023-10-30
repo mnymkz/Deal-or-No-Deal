d42615a93067620bb58b7b30a64f5c18493e473c
mnymkz <kmd2088@autuni.ac.nz>
2023-10-30 16:45:32 +1300

    feat: tabitha commits for game and gameManager

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,16 +1,107 @@
 
 package Model;
 
+import Banker.Banker;
+import Case.Case;
+import Login.User;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * model class contains all the game objects 
  * 
- * @author Michael
+ * @author Tabitha 
  */
 public class Game {
-    
     //game needs a banker
     //game needs cases
     //game needs a user 
     //game needs to get the user's first case 
     
+    private ArrayList<Case> cases;
+    private Banker banker;
+    private User user;
+    private Case firstCase;
+    
+    
+    public Game(User user, Banker banker, ArrayList<Case> cases)
+    {
+        this.user = user;
+        this.banker = banker; 
+        this.cases = cases;
+    }
+    
+    public void chooseFirstCase(int caseNumber)
+    {
+        for (Case briefCase : cases)
+        {
+            if(briefCase.getNumber() == caseNumber)
+            {
+                firstCase = briefCase;
+                break;
+            }
+        }
+    }
+    
+    public void openCase(int caseNumber)
+    {
+        for(Case briefCase : cases)
+        {
+            if(briefCase.getNumber() == caseNumber)
+            {
+                briefCase.openCase();
+                break;
+            }
+        }
+    }
+    
+    public double calculateBankerOffer()
+    {
+        double offer = banker.createOffer(cases);
+        banker.setOffer(offer);
+        return offer;        
+    }
+    
+    public boolean allCasesOpened()
+    {
+        for(Case briefCase : cases)
+        {
+            if(briefCase != firstCase && !briefCase.isOpened())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public Case getFirstCase()
+    {
+        return firstCase;
+    }
+    
+    public ArrayList<Case> getCases() {
+        return cases;
+    }
+    
+    public Banker getBanker() {
+        return banker;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+    
+    public void printRemainingCases()
+    {
+        System.out.println("User's selected case: " + firstCase.getNumber());
+        System.out.println("Remaning cases: ");
+        
+        
+        System.out.println("Banker's offer: $" + banker.getOffer());
+    }
+    
+    public void revealFirstCase()
+    {
+        System.out.println("The value of your intial briefcase is: $" + firstCase.getItem().getMoneyValue());
+    }
 }
--- src/Model/GameManager.java
+++ src/Model/GameManager.java
@@ -2,11 +2,12 @@ package Model;
 
 import Database.DBManager;
 import Login.User;
+import java.sql.SQLException;
 
 /**
  * Game manager class contains database operations for game
  *
- * @author Michael
+ * @author Tabitha
  */
 public class GameManager {
 
@@ -18,6 +19,46 @@ public class GameManager {
         this.dBManager = dBManager;
     }
 
-    //get current earnings 
+    //create a new game 
+    public void createNewGame()
+    {
+        String username = player.getUsername();
+        try
+        {
+            String query = "INSERT INTO Games (username, earnings, round) VALUES ('"+ username +"' 0, 1)";
+            dBManager.update(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
     //update current earnings 
+    public void updateCurrentEarnings(String username, double newEarnings)
+    {
+        try
+        {
+            String query = "UPDATE Games SET earnings=" + newEarnings + " WHERE username='" + username + "'";
+            dBManager.update(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    //update current round
+    public void updateCurrentRound(String username, int round)
+    {
+        try
+        {
+            String query = "UPDATE Games SET round=" + round + " WHERE username='" + username + "'";
+            dBManager.update(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
