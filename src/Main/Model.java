f8b002ec0e4a733234b31f28c12de27b54896fef
tley4 <105393760+tley4@users.noreply.github.com>
2023-10-31 00:45:42 +1300

    Creating methods for Model class

--- src/Main/Model.java
+++ src/Main/Model.java
@@ -2,15 +2,52 @@
 package Main;
 
 import Login.User;
+import Model.Game;
+import Banker.Banker;
+import java.util.ArrayList;
+import Case.Case;
 
 /**
  * model class loads game objects into the program.
  * 
- * @author Michael
+ * @author Tabitha
  */
 public class Model {
 
-    //get user from the login page
-    //load game object
+    private User user;
+    private Game game;
     
+    public Model()
+    {
+        //Initialise the game or user ?
+    }
+    
+    //create a test user 
+    
+    //load game objects
+    public void gameObjectsLoad(ArrayList<Case> cases, Banker banker)
+    {
+        this.game = new Game(user, banker, cases);
+    }
+    
+    //get user from the login page
+    public User getUserFromLogin(String username, String password)
+    {
+        //Validate user credentials?
+        // -> If validation successful, create and return a new User object
+        this.user = new User(username, password);
+        return this.user;
+    }
+    
+    //Get the current game object
+    public Game geCurrentGame()
+    {
+        return this.game;
+    }
+    
+    //Set the current game object
+    public void setCurrentGame(Game game)
+    {
+        this.game = game;
+    }
 }
