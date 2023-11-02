ae7481a003a478b1808a91cd84101ae149c0d561
mnymkz <kmd2088@autuni.ac.nz>
2023-11-02 21:31:33 +1300

    feat: added sign in action listener

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -9,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
