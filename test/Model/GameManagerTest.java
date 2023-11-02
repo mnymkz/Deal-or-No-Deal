efcf4a66621c26b29756aa0d3db3d2825b936093
mnymkz <kmd2088@autuni.ac.nz>
2023-11-03 00:46:18 +1300

    test: finished testing of GameManager class

--- test/Model/GameManagerTest.java
+++ test/Model/GameManagerTest.java
@@ -5,10 +5,11 @@
 package Model;
 
 import Database.DBManager;
+import Database.Result;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,19 +20,34 @@ import org.junit.Test;
 public class GameManagerTest {
 
     private static DBManager dBManager;
+    private static GameManager gameManager;
     
     @BeforeClass
     public static void setUpClass() throws SQLException {
         dBManager = DBManager.getInstance();
         dBManager.establishConnection(); //connect db
-        dBManager.insert("INSERT INTO player (username, password, highestEarnings) VALUES (?, ?, ?)", "testUser", "password", 100.00);
+        gameManager = new GameManager(dBManager); //new gameManager
+        dBManager.insert("INSERT INTO player (username, password, highestEarnings) VALUES (?, ?, ?)", "testUser", "password", 0.00); //create a test player
     }
     
     @AfterClass
-    public static void tearDownClass() {
+    public static void tearDownClass() throws SQLException {
+        dBManager.update("DELETE FROM PLAYER WHERE username = ?", "testUser"); //delete test player
+        dBManager.update("DELETE FROM GAME"); //clear table
+        dBManager.closeConnections(); // disconnect
     }
     
-
+    /**
+     * Test of getPlayerID method, of class GameManager.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testGetPlayerID() throws Exception {
+        System.out.println("getPlayerID");
+        int playerID = -1;
+        playerID = gameManager.getPlayerID("testUser");
+        assertNotEquals(-1, playerID); //make sure a player id is retrived from db 
+    }
 
     /**
      * Test of createNewGame method, of class GameManager.
@@ -39,25 +55,34 @@ public class GameManagerTest {
     @Test
     public void testCreateNewGame() throws Exception {
         System.out.println("createNewGame");
-
-    }
-
-    /**
-     * Test of getPlayerID method, of class GameManager.
-     */
-    @Test
-    public void testGetPlayerID() throws Exception {
-        System.out.println("getPlayerID");
-
+        //create game
+        gameManager.createNewGame("testUser");
+        int playerID = gameManager.getPlayerID("testUser"); //get player id
+        
+        //check if result not null
+        Result result = dBManager.queryDB("SELECT * FROM GAME WHERE playerID = ?", playerID);
+        ResultSet rs = result.getResultSet();
+        assertNotNull(rs); // rs should not be empty
+        assertTrue(rs.next()); //rs should have a result
     }
 
     /**
      * Test of updateCurrentEarnings method, of class GameManager.
      */
     @Test
-    public void testUpdateCurrentEarnings() {
+    public void testUpdateCurrentEarnings() throws SQLException {
         System.out.println("updateCurrentEarnings");
-
+        
+        gameManager.createNewGame("testUser"); //create new game
+        
+        //update earnings
+        double expectedEarnings = 150.00;
+        gameManager.updateCurrentEarnings("testUser", expectedEarnings); //update user current earnings
+        double actualEarnings = gameManager.getCurrentEarnings("testUser"); //get updated earnings from db
+        System.out.println(actualEarnings);
+        assertEquals(expectedEarnings, actualEarnings, 0.001); //make sure expected = actual
+        
+        gameManager.updateCurrentEarnings("testUser", 0.00); //set earnings back to default
     }
 
     /**
@@ -66,7 +91,11 @@ public class GameManagerTest {
     @Test
     public void testUpdateCurrentRound() throws Exception {
         System.out.println("updateCurrentRound");
-
+        //create new game
+        gameManager.createNewGame("testUser");
+        gameManager.updateCurrentRound("testUser", 2); //update round
+        int getCurrentRound = gameManager.getCurrentRound("testUser"); //get round
+        assertEquals(getCurrentRound, 2); //round should be 2 
     }
 
     /**
@@ -75,7 +104,10 @@ public class GameManagerTest {
     @Test
     public void testGetCurrentRound() throws Exception {
         System.out.println("getCurrentRound");
-
+        gameManager.createNewGame("testUser");
+        int round = gameManager.getCurrentRound("testUser"); //get current round
+        int roundDefault = 1; //new game starts on round 1 
+        assertEquals(round, roundDefault); //round should be 1 by default
     }
 
     /**
@@ -84,7 +116,9 @@ public class GameManagerTest {
     @Test
     public void testGetCurrentEarnings() throws Exception {
         System.out.println("getCurrentEarnings");
-
+        gameManager.createNewGame("testUser"); //create new game
+        double currentEarnings = gameManager.getCurrentEarnings("testUser"); //get current earnings
+        double defaultEarnings = 0.0;
+        assertEquals(defaultEarnings, currentEarnings, 0.01); //current earnings should be 0 by default
     }
-    
 }
