5d0a77e24ccacb08e5d9f4b08b75da3c46a3c892
mnymkz <kmd2088@autuni.ac.nz>
2023-11-02 21:30:13 +1300

    feat: added sign in action listener

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -1,11 +1,16 @@
 
 package Controller;
 
+import Login.Player;
 import Model.Model;
 import View.SignUpPanel;
 import View.View;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,20 +30,51 @@ public class SignUpController {
         this.signUpPanel.addReturnButtonActionListener(new ReturnButtonListener());
     }
     
+    /**
+     * handles sign up button click
+     */
     private class SignUpButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("Sign Up Button Clicked, creating user");
-            //TODO call create user 
+            //get player params
+            String username = signUpPanel.getUsername();
+            char[] password = signUpPanel.getPassword();
+            char[] passwordConfirm = signUpPanel.getPasswordConfirmation();
+            System.out.println(username + " " + password);
+            
+            if (Arrays.equals(password, passwordConfirm)) {
+                try {
+                    //if username exists 
+                    if (model.getLoginManager().playerExists(new Player(username, new String(password)))) {
+                        try {
+                            model.createPlayer(username, new String(password)); //create new player in model
+                            System.out.println("Player successfully created"); 
+                            mainFrame.switchPanel("GamePanel"); //switch to game panel
+                        } catch (SQLException ex) {
+                            System.out.println("Error creating new user: " + ex.getMessage());
+                            signUpPanel.setErrorMessage("Error creating new user");
+                        }
+                    }
+                } catch (SQLException ex) {
+                    System.out.println("Error checking if player exists");
+                    signUpPanel.setErrorMessage("Error checking if user exists");
+                }
+            } else {
+                 System.out.println("Passwords do not match.");
+                 signUpPanel.setErrorMessage("Passwords do not match.");
+            }
         }
     }
     
+    /**
+     * return button action listener 
+     */
     private class ReturnButtonListener implements ActionListener {
     @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("Return Button Clicked, returning");
-            //TODO call create user 
-            mainFrame.switchPanel("HomePanel");
+            mainFrame.switchPanel("HomePanel"); //return to home panel
         }
     }
     
